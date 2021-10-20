8aff7bef5ce6f40191970549748610bbb63b80ed
JUAN SOLIS <59373649+JuanMa2020@users.noreply.github.com>
2021-10-19 21:31:07 -0300

    Update install.R

--- install.R
+++ install.R
@@ -1,1 +1,7 @@
 install.packages("ggplot2")
+install.packages("rstudioapi")
+install.packages("tidyverse") 
+install.packages("stringr")
+install.packages("forecast") 
+install.packages("tseries")
+install.packages("imputeTS") 
